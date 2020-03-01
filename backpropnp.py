9765e0b92e87d7c00d5c4e252e28ce076babc85e
Wayne Radinsky <waynerad@gmail.com>
2020-02-29 18:56:05 -0700

    Improved formatting. Tested backpropnp.py on Python 3. (backproptf.py still needs to be upgraded to TensorFlow 2.0.)

--- backpropnp.py
+++ backpropnp.py
@@ -1,6 +1,9 @@
+"""The backpropagation algorithm, coded from scratch in plain Numpy"""
+
 import numpy as np
 
 def forward_propagation(X, W, B):
+    """Calculate one forward propagation pass with yhat = WX + B followed by ReLU"""
     # We are doing a linear transformation with WX + B,
     # then we are doing a ReLU with the "maximum"
     Z = np.matmul(W, X) + B
@@ -8,49 +11,52 @@ def forward_propagation(X, W, B):
     return Z, yhat
 
 def loss(A, Y):
+    """The loss, not to be confused with the cost, is just the difference between yhat (the
+    values we calculated) and Y (the correct result)"""
     return A - Y
 
-def cost(loss):
-    # squared error
-    return np.sum(loss ** 2) /(2*loss.shape[1])
+def cost(lossvalues):
+    """The cost function we are using here is the square of the error. Many different cost
+    functions can be used in machine learning."""
+    return np.sum(lossvalues ** 2) / (2 * lossvalues.shape[1])
 
 def relu_gradient_and_chain_rule(dA, Z):
-    dZ = np.array(dA, copy = True)
+    """Here we take the difference in our activation layer (dA) and, using the chain rule in
+    calculus, calculate the delta for dZ, the input into our ReLU layer. Because ReLu is
+    so simple, this calculation is very simple."""
+    dZ = np.array(dA, copy=True)
     dZ[Z <= 0] = 0
     return dZ
 
-def linear_transform_gradient_and_chain_rule(X, dZ, W, B):
+def linear_transform_gradient_and_chain_rule(X, dZ, W):
+    """Here we take the deltas from our activation layer, contained in dZ, and, using the
+    chain rule in calculus, work backwards to get the deltas for dX, dW, and dW going
+    backward through our linear transform."""
     m = X.shape[1]
     dW = (1/m) * np.dot(dZ, X.T)
-    dB = (1/m) * np.sum(dZ, axis = 1, keepdims = True)
+    dB = (1/m) * np.sum(dZ, axis=1, keepdims=True)
     dX = np.dot(W.T, dZ)
     return dX, dW, dB
 
-def both_relu_and_linear_transform_and_chain_rule(dA, Z, X, W, B):
+def both_relu_and_linear_transform_and_chain_rule(dA, Z, X, W):
+    """Here we start with the delta of our activation layer, and do our backpropagation
+    through both the ReLU non-linear activation layer and the linear (WX + B)
+    layer."""
     dZ = relu_gradient_and_chain_rule(dA, Z)
-    dX, dW, dB = linear_transform_gradient_and_chain_rule(X, dZ, W, B)
+    dX, dW, dB = linear_transform_gradient_and_chain_rule(X, dZ, W)
     return dX, dW, dB
 
-def gradient_descent_for_one_layer(W, B, X, Y, alpha, num_iterations):
-    for i in range(0, num_iterations):
-        Z, yhat = forward_propagation(X, W, B)
-        dA = loss(yhat, Y)
-        cost_value = cost(dA)
-        # print("cost", cost_value)
-        dX, dW, dB = both_relu_and_linear_transform_and_chain_rule(dA, Z, X, W, B)
-        W = W - (alpha * dW)
-        B = B - (alpha * dB)
-    return W, B
-
 def gradient_descent_for_n_layers(num_layers, W, B, X, Y, alpha, num_iterations):
+    """Here we have a multi-layer neural network. For this, W and B actually contain an
+    extra dimension so they can contain all the parameters for all the layers."""
     Z = [None] * (num_layers + 1)
     A = [None] * (num_layers + 1)
     dA = [None] * (num_layers + 1)
     dW = [None] * (num_layers + 1)
     dB = [None] * (num_layers + 1)
-    for i in range(0, num_iterations):
+    for _ in range(0, num_iterations):
 
-        A[0] = np.array(X, copy = True)
+        A[0] = np.array(X, copy=True)
         for j in range(1, num_layers + 1):
             Z[j], A[j] = forward_propagation(A[j - 1], W[j], B[j])
 
@@ -60,37 +66,41 @@ def gradient_descent_for_n_layers(num_layers, W, B, X, Y, alpha, num_iterations)
         print("cost", cost_value)
 
         for j in reversed(range(1, num_layers + 1)):
-            dA[j-1], dW[j], dB[j] = both_relu_and_linear_transform_and_chain_rule(dA[j], Z[j], A[j-1], W[j], B[j])
+            dA[j-1], dW[j], dB[j] = both_relu_and_linear_transform_and_chain_rule(
+                dA[j], Z[j], A[j-1], W[j])
             W[j] = W[j] - (alpha * dW[j])
             B[j] = B[j] - (alpha * dB[j])
 
     return W, B, yhat
 
 def initialize_parameters_for_layers(layer_list, training_examples):
+    """Uses the Gaussian (normal) distribution to initialize the parameters
+    for all the layers. It randomizes the training examples as layer 0."""
     W = [None] * len(layer_list)
     B = [None] * len(layer_list)
     X = np.random.randn(layer_list[0], training_examples)
     for j in range(1, len(layer_list)):
-        W[j] = np.random.randn(layer_list[j],layer_list[j-1])
-        B[j] = np.random.randn(layer_list[j],1)
+        W[j] = np.random.randn(layer_list[j], layer_list[j-1])
+        B[j] = np.random.randn(layer_list[j], 1)
     return X, W, B
 
 def test_fwd_propagation():
+    """Test of forward propagation and prints output to display."""
 
-    X = np.random.randn(3,5)
+    X = np.random.randn(3, 5)
     print("test", "X", X)
     print("test", "X.shape", X.shape)
-    W = np.random.randn(2,3)
+    W = np.random.randn(2, 3)
     print("test", "W", W)
     print("test", "W.shape", W.shape)
-    B = np.random.randn(2,1)
+    B = np.random.randn(2, 1)
     print("test", "B", B)
     print("test", "B.shape", B.shape)
     Z, yhat = forward_propagation(X, W, B)
     print("test", "Z", Z)
     print("test", "yhat", yhat)
 
-    ycorrect = np.arange(10).reshape(2,5)
+    ycorrect = np.arange(10).reshape(2, 5)
     print("test", "ycorrect", ycorrect)
     test_loss = loss(yhat, ycorrect)
     print("test", "test_loss", test_loss)
@@ -100,27 +110,12 @@ def test_fwd_propagation():
     # dA = A - Y
     dA = yhat - ycorrect
 
-    dX, dW, dB = both_relu_and_linear_transform_and_chain_rule(dA, Z, X, W, B)  
-    print("test","dW",dW)
-    print("test","dB",dB)
-
-def test_gradient_descent():
-
-    X = np.random.randn(3,5)
-    print("test", "X", X)
-    print("test", "X.shape", X.shape)
-    W = np.random.randn(2,3)
-    B = np.random.randn(2,1)
-
-    Y = np.arange(10).reshape(2,5)
-    print("test", "Y", Y)
-
-    alpha = 1.3
-    num_iterations = 32
-    W, B = gradient_descent(W, B, X, Y, alpha, num_iterations)
+    dX, dW, dB = both_relu_and_linear_transform_and_chain_rule(dA, Z, X, W)
+    print("test", "dW", dW)
+    print("test", "dB", dB)
 
 def test_multiple_gradient_descent_with_backprop():
-    # layer_list = [3, 2, 4, 2]
+    """Test of full gradient descent. Prints output to display."""
     layer_list = [7, 7, 7, 2]
     print("test", "layer_list", layer_list)
 
@@ -130,7 +125,7 @@ def test_multiple_gradient_descent_with_backprop():
     num_iterations = 2000
     num_layers = len(layer_list) - 1
 
-    Y = np.arange(10).reshape(2,5)
+    Y = np.arange(10).reshape(2, 5)
     W, B, yhat = gradient_descent_for_n_layers(num_layers, W, B, X, Y, alpha, num_iterations)
 
     print("test done.") # final W and B are", W, "and", B)
@@ -138,5 +133,3 @@ def test_multiple_gradient_descent_with_backprop():
 
 np.random.seed(17)
 test_multiple_gradient_descent_with_backprop()
-
-
--- backproptf.py
+++ backproptf.py
@@ -1,32 +1,45 @@
+"""This program does the same backpropagation as backpropnp.py, except instead of using
+hand-coded numpy, it uses TensorFlow. As such, it only has to implement the forward pass;
+the backward pass is figured out automatically by TensorFlow's differentiation system."""
+
 import numpy as np
 import tensorflow as tf
 
 def create_tensorflow_graph(layer_list, num_training_examples):
-    X = tf.placeholder(tf.float32, shape = [layer_list[0], num_training_examples], name="X")
+    """Here we create the TensorFlow graph for the whole neural network -- all the layers.
+    Each layer has a linear component (WX + B) and a non-linear component, which is ReLU.
+    The TensorFlow graph also includes the loss, loss squared, and cost which is the loss
+    squared divided by twice the number of training examples. Important to note that we
+    use Gaussian (normal distribution) initialization (which is called
+    glorot_normal_initializer)."""
+    X = tf.placeholder(tf.float32, shape=[layer_list[0], num_training_examples], name="X")
     A = X
     for j in range(1, len(layer_list)):
-        W = tf.get_variable("W" + str(j), shape = [layer_list[j], layer_list[j-1]], dtype = tf.float32, initializer = tf.glorot_normal_initializer())
-        B = tf.get_variable("B" + str(j), shape = [layer_list[j], 1], dtype = tf.float32, initializer = tf.glorot_normal_initializer())
+        W = tf.get_variable("W" + str(j), shape=[layer_list[j], layer_list[j-1]],
+                            dtype=tf.float32, initializer=tf.glorot_normal_initializer())
+        B = tf.get_variable("B" + str(j), shape=[layer_list[j], 1], dtype=tf.float32,
+                            initializer=tf.glorot_normal_initializer())
         # Z = WA + B (remember A = X for 1st later, so Z = WX + B)
         Z = tf.add(tf.matmul(W, A), B, name="Z" + str(j))
-        A = tf.nn.relu(Z, name = "A" + str(j))
+        A = tf.nn.relu(Z, name="A" + str(j))
 
     yhat = A
-    Y = tf.placeholder(tf.float32, shape = yhat.shape, name = "Y")
-    loss = tf.subtract(yhat, Y, name = "loss")
-    loss_squared = tf.square(loss, name = "loss_squared")
+    Y = tf.placeholder(tf.float32, shape=yhat.shape, name="Y")
+    loss = tf.subtract(yhat, Y, name="loss")
+    loss_squared = tf.square(loss, name="loss_squared")
     thesum = tf.reduce_sum(loss_squared, name="thesum")
     number_of_training_examples = int(yhat.shape[1])
     cost = tf.divide(thesum, 2.0 * number_of_training_examples, name="cost")
     return cost, X, yhat, Y
 
 def test_backprop_with_tensorflow():
+    """This is our entry point to test the whole system. We construct the layer list,
+    initialize by randomizing the training examples, and tell TensorFlow's
+    GradientDescentOptimizer to minimize the cost function with learning rate alpha."""
 
     np.random.seed(17)
     tf.set_random_seed(17)
-    # layer_list = [3, 2, 4, 2]
-    # layer_list = [20, 20, 20, 2]
-    layer_list = [7,7,7,2]
+    layer_list = [7, 7, 7, 2]
     print("test", "layer_list", layer_list)
     num_training_examples = 5
 
@@ -35,7 +48,7 @@ def test_backprop_with_tensorflow():
     X_val = np.random.randn(layer_list[0], num_training_examples)
     print("test", "X_val", X_val)
 
-    Y_val = np.arange(10).reshape(2,5)
+    Y_val = np.arange(10).reshape(2, 5)
     print("test", "Y_val", Y_val)
 
     alpha = 0.01
@@ -50,7 +63,9 @@ def test_backprop_with_tensorflow():
         num_prints = 5
         for i in range(num_iterations):
 
-            train_op_value, cost_value, yhat_value = sess.run([train_op, cost, yhat], feed_dict={X_node: X_val, Y_node: Y_val})
+            train_op_value, cost_value, yhat_value = sess.run([train_op, cost, yhat],
+                                                              feed_dict={X_node: X_val,
+                                                                         Y_node: Y_val})
             print(i, "cost", cost_value)
             if i % int(num_iterations/num_prints) == 0:
 
@@ -59,4 +74,3 @@ def test_backprop_with_tensorflow():
         print("yhat", yhat_value)
 
 test_backprop_with_tensorflow()
-
